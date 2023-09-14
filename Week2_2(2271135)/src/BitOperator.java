95bcc5c525ee770f43db2634865181c4b50fce59
reneejoo22 <renee.joo03@gmail.com>
2023-09-14 23:20:19 +0900

    제발 업로드돼라

---
+++ Week2_2(2271135)/src/BitOperator.java
@@ -0,0 +1,25 @@
+//2주차_예제2-?
+
+public class BitOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte c = 20;
+		byte d = -8;
+	
+		System.out.println("[시프트 연산 결과]");
+		System.out.println(c<<2);
+		System.out.println(c>>2);
+		System.out.println(d>>2);
+		System.out.printf("%x\n",(d>>>2));
+		
+		/*
+		System.out.printf("%04x\n",(short)(a&b));
+		System.out.printf("%04x\n",(short)(a|b));
+		System.out.printf("%04x\n",(short)(a^b));
+		System.out.printf("%04x\n",(short)(~a));
+*/
+	}
+
+}
