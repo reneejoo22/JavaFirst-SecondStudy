d2d37a9a7cee0a2e35d059cf62ce575d2459e59e
reneejoo22 <renee.joo03@gmail.com>
2023-09-17 15:24:01 +0900

    2-9예제 작성

--- Week2_2(2271135)/src/BitOperator.java
+++ Week2_2(2271135)/src/BitOperator.java
@@ -1,9 +1,19 @@
-//2주차_예제2-?
+//2주차_예제2-9
 
 public class BitOperator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		short a = (short)0x55ff;
+		short b = (short)0x00ff;
+		
+		//비트 논리 연산
+		System.out.println("[비트연산결과]");
+		System.out.printf("%04x\n",(short)(a&b));	//비트 and_ 11일때만 참
+		System.out.printf("%04x\n",(short)(a|b));	//비트 or_ 00제외 전부 참
+		System.out.printf("%04x\n",(short)(a^b));	//비트 xor_ 01이거나 10일때만 참
+		System.out.printf("%04x\n",(short)(~a));	//비트 not_
+		
 		
 		byte c = 20;
 		byte d = -8;
@@ -15,7 +25,7 @@ public class BitOperator {
 		System.out.printf("%x\n",(d>>>2));
 		
 		/*
-		수정
+		second sample
 		*/
 	}
 
